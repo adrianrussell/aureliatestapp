0d94d7941c793213b977f3fa905aecaf8bbac301
AdrianRussell <adrian.f.russell@gmail.com>
2016-08-12 12:44:48 +1000

    move install of aurelia up

---
+++ ata/deploy.cmd
@@ -0,0 +1,34 @@
+
+:: 2. Select node version
+
+call :SelectNodeVersion
+echo "Starting Aurelia Command line install"
+:: 3. Install the aurelia command line
+
+call :ExecuteCmd !NPM_CMD! install aurelia-cli -g
+echo "Aurelia Command line install Complete"
+
+
+:: 1. KuduSync
+
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\src" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+)
+
+:: 4. Install npm packages
+
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+
+  pushd "%DEPLOYMENT_TARGET%"
+
+  call :ExecuteCmd !NPM_CMD! install
+
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  popd
+
+)
